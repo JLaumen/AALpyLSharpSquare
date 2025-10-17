028ef94a9cd324770eb6b12cc7e5b384dc5983a0
JLaumen <96011116+JLaumen@users.noreply.github.com>
2025-10-17 04:40:35 +0200

    Fix

--- aalpy/learning_algs/deterministic/ObservationTreeSquare.py
+++ aalpy/learning_algs/deterministic/ObservationTreeSquare.py
@@ -432,6 +432,7 @@ class ObservationTreeSquare:
         cex_outputs, _ = self._get_output_sequence(cex_inputs, query_mode="full")
         print("Obtained outputs:", cex_outputs)
         self.insert_observation_sequence(cex_inputs, cex_outputs)
+        print(self.root)
 
 
     def _get_output_sequence(self, inputs, query_mode="full"):
